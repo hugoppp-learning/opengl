314fc2fd40f1158b44aa7cc26ab7c6ba3a373f32
Hugo Protsch <hugo.protsch@gmail.com>
2021-02-25 13:36:53 +0100

    Add file and line to msg

--- src/myAssert.hpp
+++ src/myAssert.hpp
@@ -1,13 +1,17 @@
 
 #pragma once
 
+#include <iostream>
 
 #ifdef NDEBUG
 #define ASSERT(x) x
 #define ASSERT_MSG(x, msg) x
 #else
 #define ASSERT(x) if (!(x)) __debugbreak()
-#define ASSERT_MSG(x, msg) if (!(x)){ \
-std::cout << msg << std::endl;                \
+#define ASSERT_MSG(x, msg) if (!(x)){                                                           \
+std::cout                                                                                       \
+<< "\nERROR: Assert failed in '"  << __FUNCTION__ << "' in file\n"                              \
+<<  __FILE__ << ":" << __LINE__ << "\n'"                                                        \
+<< msg << "'"<< std::endl;                                                                      \
 __debugbreak();}
 #endif
